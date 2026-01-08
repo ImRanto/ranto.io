02607b51325882164012d5e76e942693bbc60452
h-ranto <hei.ranto.2@gmail.com>
2026-01-08 19:09:00 +0300

    feat: add ExperienceSection to Home component layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,18 @@ import ProjectsSection from "@/components/project-section";
 import ContactSection from "@/components/contact-section";
 import Footer from "@/components/footer";
 import Navbar from "@/components/Navbar";
+import { ExperienceSection } from "@/components/ExperienceSection";
 
 export default function Home() {
   return (
-    <div >
-      <Navbar/>
-      <HeroSection/>
-      <AboutSection/>
-      <ProjectsSection/>
-      <ContactSection/>
-      <Footer/>
+    <div>
+      <Navbar />
+      <HeroSection />
+      <AboutSection />
+      <ProjectsSection />
+      <ExperienceSection />
+      <ContactSection />
+      <Footer />
     </div>
   );
 }
